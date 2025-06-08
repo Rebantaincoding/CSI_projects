70ab8c8b5ef851297931f728a97e8a2a329efdcd
Rebantaincoding <142164448+Rebantaincoding@users.noreply.github.com>
2025-06-08 11:53:48 +0530

    Add files via upload

---
+++ RebantaSarkar_assign1.py
@@ -0,0 +1,24 @@
+#Rebanta Sarkar
+#Celebal user_id:CT_CSI_DS_4841 
+#Program to print Lower triangle,Upper Triangle and Pyramid 
+print("\n Enter size of triange \n")
+n=int(input())
+print("\nLower Triangle:\n")
+for i in range (0,n):
+    for j in range(0,i+1):
+        print('*',end='')
+    print() 
+print("\nUpper triangle:\n")
+for i in range(0,n):
+    for j in range(0,i+1):
+        print(' ',end='') 
+    for k in range(n-i,0,-1):
+        print('*',end='') 
+    print()
+print("\nPyramid:\n")
+for i in range(0,n):
+    for j in range(n-i,0,-1):
+        print(' ',end='')
+    for k in range(0,i+1):
+        print('* ',end='')
+    print()
